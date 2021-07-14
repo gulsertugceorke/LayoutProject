9d7eca5a20526db6412318677f1b0a318a2593ba
Tuğçe Örke <80844937+gulsertugceorke@users.noreply.github.com>
2021-07-14 13:40:20 +0300

    Update MainActivity.kt

--- src/main/java/com/example/devprofile/MainActivity.kt
+++ src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var logo1: ImageView =findViewById(R.id.logo)
+
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.logo5)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+       rounded.cornerRadius = 15f
+       // rounded.isCircular = true //logonun yuvarlak olması için
+        logo1.setImageDrawable(rounded)
+
     }
 }
